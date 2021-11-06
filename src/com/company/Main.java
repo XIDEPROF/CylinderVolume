3bf999bb0668b97d85831b0cc5e67cca1a36a654
Serge <78102903+MrXides@users.noreply.github.com>
2021-11-06 03:50:06 +0300

    t-1
    t

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Вычисление объема цилиндра
+	System.out.println("Объем цилиндра равен " + (3.1416*4*4*5));
+    }
+}
